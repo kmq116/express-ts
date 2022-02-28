2411088032fc8a08636040a9ab1057aa0136ddf5
kmq116 <coderco@foxmail.com>
2022-02-28 22:19:55 +0800

    引入 socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
+import * as http from 'http'
 import express from 'express'
 import cors from 'cors'
+import { Server } from 'socket.io'
 import { dbListeners } from './mongodb/db'
 import useRouter from './routers'
 // import swaggerUi from 'swagger-ui-express'
-dbListeners()
+
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+useRouter(app)
+dbListeners()
 app.use(cors()).use(express.static('public'))
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
-useRouter(app)
 const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-console.warn(`server started on port http://127.0.0.1:${PORT}`)
-app.listen(PORT)
+
+io.on('connection', () => {
+  console.warn('a user connected')
+})
+
+server.listen(3000, () => {
+  console.warn(`server started on port http://127.0.0.1:${PORT}`)
+})
