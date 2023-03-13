d9111ee1a878df22979f024b8a13020e7fd45a15
飞天御剑流co <521luqiya@gmail.com>
2023-03-13 22:33:02 +0800

    fix: 修复 req.body 空的问题（执行顺序）

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,31 @@
-import * as http from 'http'
-import express from 'express'
-import cors from 'cors'
-import { Server } from 'socket.io'
-import { dbListeners } from './mongodb/db'
-import useRouter from './routers'
+import * as http from "http";
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
+import { dbListeners } from "./mongodb/db";
+import useRouter from "./routers";
 // import swaggerUi from 'swagger-ui-express'
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
-useRouter(app)
-dbListeners()
-app.use(cors()).use(express.static('public'))
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-const PORT = process.env.PORT || 3000
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-io.on('connection', () => {
-  console.warn('a user connected')
-})
+dbListeners();
+app
+  .use(cors())
+  .use(express.static("public"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }));
+useRouter(app);
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+io.on("connection", () => {
+  console.warn("a user connected");
+});
 
 server.listen(3000, () => {
-  console.warn(`server started on port http://127.0.0.1:${PORT}`)
-})
+  console.warn(`server started on port http://127.0.0.1:${PORT}`);
+});
